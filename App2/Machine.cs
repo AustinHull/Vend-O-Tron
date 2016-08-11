f9aef8bff3d7bad977897d5beb26d9964e056ecf
AustinHull <austinplaneboy@yahoo.com>
2016-08-11 10:16:38 -0400

    Adding various pieces of groundwork for future SQLite functionality.

---
+++ App2/Machine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace App2
+{
+    public class Machine
+    {
+        public int machineID { get; set; }
+        public string machineName { get; set; }
+        public string machineLocation { get; set; }
+    }
+}
--- App2/MainPage.xaml.cs
+++ App2/MainPage.xaml.cs
@@ -16,9 +16,11 @@ using Windows.UI.Input;
 using Windows.Devices.Input;
 using Windows.UI.Core;
 using System.Diagnostics;
-using C = System.Data.Common;
+using System.Data.Common;
 using Windows.Storage;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.Data.Entity;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -43,12 +45,6 @@ namespace App2
             Task tempFile;
 
             tempFile = loadFile("dataBase.txt");
-
-
-            //C.DbConnection dataBase;
-
-
-            //dataBase.ConnectionString = "Server=tcp:YOUR_SERVER_NAME_HERE.database.windows.net,1433;Database=AdventureWorksLT;User ID=";
 
             this.VendFinderApp.PointerPressedOverride += VendFinderApp_PointerPressedOverride;
 
@@ -77,10 +73,15 @@ namespace App2
             inputPrompt.Visibility = Visibility.Visible;
             VendFinderApp.Children.Add(inputPrompt);
 
+            // Generic file name for file which stores user-prescribed location info.
             string name = "database.txt";
 
+            // Package the info file and save it to the user's system.
             Windows.Storage.StorageFile storeFile =
-                await storageFolder.CreateFileAsync("dataBase.txt", CreationCollisionOption.GenerateUniqueName);
+                await storageFolder.CreateFileAsync(name, CreationCollisionOption.GenerateUniqueName);
+
+            // Try putting some SQLite stuff here...
+
 
             storeFile = await storageFolder.GetFileAsync("dataBase.txt");
             await Windows.Storage.FileIO.WriteTextAsync(storeFile, loc.Latitude.ToString() + " " + loc.Longitude.ToString());
@@ -92,15 +93,13 @@ namespace App2
         {
             while (true)
             {
-                
-
-
                 Debug.WriteLine("Hello");
                 //UInt32 pointerLoc = 0;
                 //PointerPoint.GetCurrentPoint(pointerLoc);
             }
         }
 
+        // This is the task which runs to save a specified file to the user system.
         public async Task<StorageFile> loadFile(string fileName)
         {
             Windows.Storage.StorageFile loadFile = await storageFolder.GetFileAsync(fileName);
